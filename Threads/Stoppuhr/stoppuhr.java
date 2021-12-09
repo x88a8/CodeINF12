908e24fcd0c88877e1fd5b8b3296d7ba06339995
Bruno Thalmann <77811859+x88a8@users.noreply.github.com>
2021-12-09 11:50:36 +0100

    Update stoppuhr.java

--- Threads/Stoppuhr/stoppuhr.java
+++ Threads/Stoppuhr/stoppuhr.java
@@ -1,1 +1,17 @@
-
+public class stoppuhr extends Thread {
+  private SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+  
+  public void run() {
+    while (!isInterrupted()) {
+      Date time = new Date();
+      System.out.println(sdf.format(time));
+      try {
+        sleep(1000);
+      }
+      catch(InterruptedException e) {
+        System.out.println(e.getMessage());
+        interrupt();
+      }
+    }
+  }
+}
